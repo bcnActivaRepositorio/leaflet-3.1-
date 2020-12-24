c7d802d41e4c5348d77422beace220ef5e637815
Roberto Garcia <41040915+naranjito72@users.noreply.github.com>
2020-12-24 20:30:45 +0100

    Update apiRestaurants.php

--- api/apiRestaurants.php
+++ api/apiRestaurants.php
@@ -1,14 +1,8 @@
 <?php
-<<<<<<< HEAD
+
 
 // header('Access-Control-Allow-Origin: http://localhost/mapa/api/apiRestaurants.php');
 header('Access-Control-Allow-Origin: http://localhost:3000/bares');
-=======
-/*
-Trataremos los datos deacuerdo con la especificación 
-*/
-header('Access-Control-Allow-Origin: http://localhost/mapa/api/apiRestaurants.php');
->>>>>>> 56f2e815ab4bfc2fd6cc5e534ee93b5da630bd1e
 include("_db.php");
 // call to database
 $db = ("mysql:host=localhost;dbname=restaurants");
